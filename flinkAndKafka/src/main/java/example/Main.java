e151db35d69cc4e7099273c1ef9af9b50f360080
ldentis <ldenti@arcese.com>
2024-01-31 11:21:58 +0100

    averages are now written on sqlserver db instead of being printed

--- flinkAndKafka/src/main/java/example/Main.java
+++ flinkAndKafka/src/main/java/example/Main.java
@@ -7,22 +7,21 @@ import org.apache.flink.api.common.serialization.SerializationSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
-import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.triggers.CountTrigger;
 import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.api.windowing.windows.Window;
 import org.apache.flink.util.Collector;
 import users.User;
-
-import java.util.NoSuchElementException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -31,16 +30,19 @@ public class Main {
         final ParameterTool params = ParameterTool.fromArgs(args);
         env.getConfig().setGlobalJobParameters(params);
 
+        //KAFKA SOURCES, creation of singleton
         KafkaSources kafkaSources = KafkaSources.getInstance();
 
+        //data streams, getting sources from singleton
         DataStream<User> userDataStream =  env.fromSource(kafkaSources.getUserSource(), WatermarkStrategy.noWatermarks(), "Kafka User Source");
         DataStream<Purchase> purchaseDataStream =  env.fromSource(kafkaSources.getPurchaseSource(), WatermarkStrategy.forMonotonousTimestamps(), "Kafka Purchases Source")
-        .assignTimestampsAndWatermarks(WatermarkStrategy.<Purchase>forMonotonousTimestamps().withTimestampAssigner((event, timestamp) -> event.getEventTimeMillis()));
+        .assignTimestampsAndWatermarks(WatermarkStrategy.<Purchase>forMonotonousTimestamps().withTimestampAssigner((event, timestamp) -> event.getEventTimeMillis())); //setting watermarks as monotonous, timestamps are written on object
 
+        //merge user and purchase streams
         DataStream<TransportOrder> orderStream = userDataStream.coGroup(purchaseDataStream)
                 .where(User::getId)
-                .equalTo(Purchase::getUid)
-                .window(GlobalWindows.create()).trigger(CountTrigger.of(1))
+                .equalTo(Purchase::getUid) //merge on uid
+                .window(GlobalWindows.create()).trigger(CountTrigger.of(1))//new transport order for each combination of user and purchase (with same uid), so trigger on each element
                 .apply(new CoGroupFunction<User, Purchase, TransportOrder>() {
                     @Override
                     public void coGroup(Iterable<User> first, Iterable<Purchase> second, Collector<TransportOrder> out) throws Exception {
@@ -53,11 +55,12 @@ public class Main {
                     }
                 });
 
+        //kafka sink for transport orders
         KafkaSink<TransportOrder> transportOrderSink = KafkaSink.<TransportOrder>builder()
         .setBootstrapServers("kafka:9092")
         .setRecordSerializer(KafkaRecordSerializationSchema.builder()
             .setTopic("transportOrders")
-            .setValueSerializationSchema(new SerializationSchema<TransportOrder>() {
+            .setValueSerializationSchema(new SerializationSchema<TransportOrder>() { //custom serialization schema, simply toString and then .bytes
                 @Override
                 public byte[] serialize(TransportOrder element) {
                     return element.serialize();
@@ -72,9 +75,35 @@ public class Main {
 
         orderStream.sinkTo(transportOrderSink);
 
+        SinkFunction<Tuple2<Integer,Double>> sqlSink = JdbcSink.sink( //no comment, sqlserver è terribile
+                "MERGE averages AS target\n" +
+                        "USING (VALUES (?, ?)) AS source (uid, average)\n" +
+                        "ON (target.uid = source.uid)\n" +
+                        "WHEN MATCHED THEN \n" +
+                        "    UPDATE SET average = source.average\n" +
+                        "WHEN NOT MATCHED THEN \n" +
+                        "    INSERT (uid, average) \n" +
+                        "    VALUES (source.uid, source.average);", (ps, t) -> {
+                ps.setInt(1, t.f0);
+                ps.setDouble(2, t.f1);
+                },
+                JdbcExecutionOptions.builder()
+                                .withBatchSize(1000)
+                                .withBatchIntervalMs(200)
+                                .withMaxRetries(5)
+                                .build(),
+                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                                .withUrl("jdbc:sqlserver://sqlserver:1433;databaseName=FlinkDB")
+                                .withDriverName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+                                .withUsername("sa")
+                                .withPassword("Pass@Word")
+                                .build()
+
+        );
+
         purchaseDataStream
                 .keyBy(Purchase::getUid)
-                .countWindow(10).process(new GenericPurchaseAveragedCount<GlobalWindow>()).print("Count Window");
+                .countWindow(10).process(new GenericPurchaseAveragedCount<GlobalWindow>()).addSink(sqlSink);
 
         /*
         purchaseDataStream.
